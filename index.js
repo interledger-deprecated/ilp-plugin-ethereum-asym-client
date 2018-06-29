ac17e9c73dcc8b2696c5da4ae405337e267f039a
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-06-29 10:54:53 -0400

    Change asset scale to 9, gwei

--- index.js
+++ index.js
@@ -16,13 +16,51 @@ async function _requestId () {
   })
 }
 
+class EthUnit extends BigNumber {
+  constructor (amount, unit) {
+    super(amount)
+    if (!unit) {
+      throw new Error('Must specify a unit of ether')
+    }
+    this.unit = unit
+  }
+
+  _transform (unit) {
+    const orders = {
+      wei: 0,
+      gwei: 9,
+      ether: 18
+    }
+    return new EthUnit(
+      this.shift(orders[this.unit] - orders[unit]),
+      unit
+    )
+  }
+
+  wei () {
+    return this._transform('wei')
+  }
+
+  gwei () {
+    return this._transform('gwei')
+  }
+
+  ether () {
+    return this._transform('ether')
+  }
+
+  ethStr () {
+    return this.ether().toString() + ' ETH'
+  }
+}
+
 class Plugin extends PluginBtp {
   constructor (opts) {
     super(opts)
     this._account = opts.account
     this._db = opts.db || 'machinomy_db'
     this._provider = opts.provider || 'http://localhost:8545'
-    this._minimumChannelAmount = new BigNumber(opts.minimumChannelAmount || 100)
+    this._minimumChannelAmount = new EthUnit(opts.minimumChannelAmount || 1, 'gwei')
     this._web3 = new Web3(typeof this._provider === 'string'
       ? new Web3.providers.HttpProvider(this._provider)
       : this._provider)
@@ -32,7 +70,7 @@ class Plugin extends PluginBtp {
   async _connect () {
     this._machinomy = new Machinomy(this._account, this._web3, {
       databaseUrl: 'nedb://' + this._db,
-      minimumChannelAmount: this._minimumChannelAmount
+      minimumChannelAmount: this._minimumChannelAmount.wei()
     })
     const infoResponse = await this._call(null, {
       type: BtpPacket.TYPE_MESSAGE,
@@ -51,12 +89,14 @@ class Plugin extends PluginBtp {
     const result = await this._machinomy.channelManager.requireOpenChannel(
       this._account,
       this._peerAccount,
-      this._minimumChannelAmount)
+      this._minimumChannelAmount.wei()
+    )
 
-    debug('got payment channel. channelId=' + result.channelId,
-      'value=' + result.value.toString(),
-      'spent=' + result.spent.toString(),
-      'receiver=' + result.receiver)
+    const format = n => new EthUnit(n, 'wei').ethStr()
+    debug('got payment channel. channelId=', result.channelId,
+      `spent ${format(result.spent)} / ${format(result.value)}`,
+      'receiver=', result.receiver
+    )
     this._channel = result.channelId
     this._receiver = result.receiver
 
@@ -76,14 +116,17 @@ class Plugin extends PluginBtp {
     const channels = (await this._machinomy.channels()).filter(
       c => c.receiver === this._account && c.state === 0
     )
-    debug('settling payment channels for amounts', channels.map(c => c.spent.toString()))
     return Promise.all(
-      channels.map(c => this._machinomy.close(c.channelId))
+      channels.map(c => {
+        debug('settling payment channel for', new EthUnit(c.spent, 'wei').ethStr())
+        return this._machinomy.close(c.channelId)
+      })
     )
   }
 
   async _handleData (from, { requestId, data }) {
     const { ilp } = this.protocolDataToIlpAndCustom(data)
+
     if (!this._dataHandler) {
       throw new Error('no request handler registered')
     }
@@ -93,7 +136,7 @@ class Plugin extends PluginBtp {
   }
 
   async sendMoney (amount) {
-    const price = new BigNumber(amount)
+    const price = new EthUnit(amount, 'gwei')
 
     // Don't bother sending channel updates for 0 amounts
     if (price.eq(0)) {
@@ -105,9 +148,11 @@ class Plugin extends PluginBtp {
       .filter(c => c.channelId === this._channel)[0]
 
     // Deposit enough to the existing channel to cover the payment instead of opening a new one
-    const depositAmount = price.plus(currentChannel.spent).minus(currentChannel.value)
+    const depositAmount = price.wei()
+      .plus(currentChannel.spent)
+      .minus(currentChannel.value)
     if (depositAmount.gt(0)) {
-      debug('funding channel for', depositAmount.toString())
+      debug(`funding channel for ${depositAmount.ethStr()}`)
       await this._machinomy.deposit(this._channel, depositAmount)
     }
 
@@ -117,7 +162,8 @@ class Plugin extends PluginBtp {
       price
     })
 
-    debug('sending payment.', payment, JSON.stringify(payment))
+    // TODO: how should I debug this?
+    debug('sending payment.', payment)
     return this._call(null, {
       type: BtpPacket.TYPE_TRANSFER,
       requestId: await _requestId(),
@@ -139,10 +185,15 @@ class Plugin extends PluginBtp {
       const payment = JSON.parse(primary.data.toString())
       await this._machinomy.acceptPayment({ payment })
 
-      debug('got payment. amount=' + payment.price)
-
-      if (new BigNumber(payment.price).gt(0) && this._moneyHandler) {
-        await this._moneyHandler(payment.price.toString())
+      const amount = new EthUnit(payment.price, 'wei').gwei()
+      debug('got payment for', amount.ethStr())
+      
+      if (!this._moneyHandler) {
+        throw new Error('no money handler registered')
+      }
+      
+      if (amount.gt(0)) {
+        await this._moneyHandler(amount.toString())
       }
     }
   }
